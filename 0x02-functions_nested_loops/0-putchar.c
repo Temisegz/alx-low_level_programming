7ed678881422711f42007815e52e2cb0d1320735
Temisegz <temiloluwasegun54@gmail.com>
2022-07-15 01:00:11 -0700

    fuction nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/88
+ main - Prints Holberton
+
+ Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('l');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
