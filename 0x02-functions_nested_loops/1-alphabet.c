75c3d8ee645d21bdb0b67c17a49678dc2345ad1a
Temisegz <temiloluwasegun54@gmail.com>
2022-07-15 20:44:51 -0700

    FNL

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,16 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alp = 'a';
-
-	while (alp <= 'z');
-
+		
+	while (alp <= 'z')
 	{
 		_putchar (alp);
-		alp++;
+		alp++;					
 
 	}
 	_putchar ('\n');
-
+				
 }
