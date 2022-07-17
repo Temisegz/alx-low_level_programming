e010c0968bb89a17fc843f1df6f5aa3684ed7b5a
Temisegz <temiloluwasegun54@gmail.com>
2022-07-17 11:18:45 -0700

    functuon nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,17 +8,16 @@
  *      * Return: Always 0.
  *       */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122);
-	{
 
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
 	}
-		else if (c >= 65 && c <= 90);
-
-		{
-			return (1);	
-		}
-			return (0);
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
