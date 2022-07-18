506a60adc28878dabaebddc90d0f5361c60f82e2
Temisegz <temiloluwasegun54@gmail.com>
2022-07-17 20:11:19 -0700

    function nested loops

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet();
+	int r;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
 	return (0);
 }
