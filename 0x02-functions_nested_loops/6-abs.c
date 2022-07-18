6e82ef8dc7f3d3b2cab6a75363e3634f13a1d56b
Temisegz <temiloluwasegun54@gmail.com>
2022-07-17 22:50:15 -0700

    funtion nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,16 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ibs - returns absolute value of an int
+ * @n: integer from which to get an absolute value
+*  * Return: Void.
+*/
 
-int _abs(int);
-
+int _abs(int n)
 {
-	int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
 }
