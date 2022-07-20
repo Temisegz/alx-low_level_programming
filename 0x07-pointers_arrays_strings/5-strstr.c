7ab62391bd5d36dd14ee662f56cd4c3658be07eb
Temisegz <temiloluwasegun54@gmail.com>
2022-07-19 22:29:59 -0700

    Even more Pointer array strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strstr - function locate
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: 0
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	char *result = haystack, *fneedle = needle;
