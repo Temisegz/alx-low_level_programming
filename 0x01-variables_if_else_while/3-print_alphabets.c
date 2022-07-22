e3c183eddfd062e6a126b91023752aef8cc3869c
Temisegz <temiloluwasegun54@gmail.com>
2022-07-21 21:11:14 -0700

    x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,15 @@
  *   *
  *    * Return: Always 0.
  */
-int main(void)
+nt main(void)
 {
 	char letter;
 
-	for (letter = a; letter <= z; letter++)
-	putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-	for (letter = A; letter <= Z; letter++)
-	putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
 	putchar('\n');
 
