61e2f749cb32b300bd6b71e1b98deb4a8abbd526
Temisegz <temiloluwasegun54@gmail.com>
2022-07-21 21:55:35 -0700

    x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,42 +8,21 @@
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
-
-	while (p < 58)
+	int num1, num2;
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		while (q < 58)7
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
 	putchar('\n');
 	return (0);
