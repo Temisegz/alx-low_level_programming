3d0c815e1d9c7db230df135170a52443369f78e4
Temisegz <temiloluwasegun54@gmail.com>
2022-07-22 19:57:37 -0700

    0x09 static libraries

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,17 +3,16 @@
 /**
  * _strlen - length of a string
  * @s: input char
+ *
  * Return: length of a string
  */
 
 int _strlen(char *s)
 {
-	int l = 0;
+	int len = 0;
 
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
+	while (*s++)
+		len++;
+
+	return (len);
 }
