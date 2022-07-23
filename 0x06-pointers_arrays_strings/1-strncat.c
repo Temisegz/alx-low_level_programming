1779f01ffc1b2abf88fe5f76a33b7775d6622bf5
Temisegz <temiloluwasegun54@gmail.com>
2022-07-22 19:07:02 -0700

    0x06 Low level P

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat - two words
- * @dest : pointer to char param
- * @src : pointer to char param
- * @n : int parameter
- * Return: *dest
+ *  * _strncat - two words
+ *   * @dest : pointer to char param
+ *    * @src : pointer to char param
+ *     * @n : int parameter
+ *      * Return: *dest
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -23,10 +23,11 @@ char *_strncat(char *dest, char *src, int n)
 		}
 		m++;
 	}
+
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[m + i] = src[i];
 	}
 	dest[m + i] = '\0';
-	return (dest)
+	return (dest);
 }
