e492b37da691830df26054dfe67140654543fb9e
Temisegz <temiloluwasegun54@gmail.com>
2022-08-04 21:48:16 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(arguments, n);
 
+	for (s = 0; s < n; s++)
 	{
 		clone = va_arg(arguments, char*);
 		if (clone != NULL)
